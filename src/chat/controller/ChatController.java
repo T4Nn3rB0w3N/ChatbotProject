5df2f800802d76452aa2790f84667327be2b8a12
Bowen <tbow1382@740s-pl-120.local>
2018-10-12 09:48:08 -0600

    imported Chatbot model, javax, and scanner into controller, as well as created a way for user to exit through input;  created runner; created data members, methods, and getters in Chatbot model;

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,6 +1,29 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+
 public class ChatController
 {
-
+	
+	
+	public void start()
+	{
+		String userInput = "";
+		while (!userInput.equalsIgnoreCase("quit"))
+			{
+				userInput = JOptionPane.showInputDialog(null, "type \"quit\" to exit");
+				//user will now know how to exit the chatbot
+				
+			}
+	}
+	
+	public ChatController()
+	{
+		
+	}
+	
+	
 }
--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -1,6 +1,52 @@
 package chat.model;
 
+import java.util.ArrayList;
+
 public class Chatbot
 {
-
+	//------data members
+	private ArrayList<String> responseList;
+	private String currentUser;
+	private String joke;
+	private String content;
+	private ArrayList<String> spookyList;
+	
+	public Chatbot()
+	{
+		
+	}
+	
+	public Chatbot(ArrayList<String> responseList, String currentUser, String joke, String content, ArrayList<String> spookyList)
+	{
+		this.responseList = responseList;
+		this.currentUser = currentUser;
+		this.joke = joke;
+		this.content = content;
+		this.spookyList = spookyList;
+	}
+	
+	public ArrayList<String>  getResponseList()
+	{
+		return responseList;
+	}
+	
+	public String getCurrentUser()
+	{
+		return currentUser;
+	}
+	
+	public String getJoke()
+	{
+		return joke;
+	}
+	
+	public String getContent()
+	{
+		return content;
+	}
+	
+	public ArrayList<String> getSpookyList()
+	{
+		return spookyList;
+	}
 }
