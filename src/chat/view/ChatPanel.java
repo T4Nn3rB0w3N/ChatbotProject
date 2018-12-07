0083b7023c814db06e9ac579ec10798f311285d1
Bowen <tbow1382@740s-pl-120.local>
2018-12-07 08:32:01 -0700

    Created ChatPanel without any buttons or labels or the like

---
+++ src/chat/view/ChatPanel.java
@@ -0,0 +1,36 @@
+package chat.view;
+
+import javax.swing.*;
+import chat.controller.ChatController;
+
+public class ChatPanel extends JPanel
+{
+	private ChatController theController;
+	
+	public ChatPanel(ChatController theController)
+	{
+		super();
+		this.theController = theController;
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupPanel()
+	{
+		
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+}
